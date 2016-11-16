a437419555e10b3ec46b58ee75da7a72ae62b637
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-11-15 22:32:04 -0700

    add basic append function

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var $site = $('.website');
 var $link = $('.url');
 var $save = $('.save');
 
-$('.save').on('click', function(event) {
+$($save).on('click', function(event) {
   event.preventDefault();
   console.log('hi');
-})
+  $('.bookmark').append(`<h2>${$site}</h2>
+    <h3>${$link}</h3>
+    <button type="button" name="read">Mark as Read</button>
+    <button type="button" name="remove">Remove</button>`)
+});
