ca12bd8cb0227004c1c61de92de6f66a697f9d9a
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-11-18 17:13:06 -0700

    WIP read button and delete button

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 var $site = $('.website');
 var $link = $('.url');
 var $save = $('.save');
+var $list = [];
 
-$($save).on('click', function(event) {
-  event.preventDefault();
-
+$($save).on('click', function() {
   $('.bookmark').append(`<section class="newbookmark"><h2>${$site.val()}</h2>
     <h3>${$link.val()}</h3>
     <button type="button" name="mark" class="mark">Read</button>
-    <button type="button" name="remove" class="remove">Delete</button></section>`);
+    <button type="button" name="remove" class="delete">Delete</button></section>`);
+
+  $($list).push();
 
   $($site).val('');
 
   $($link).val('');
 
-  $('.mark').on('click', function(event) {
-    $('section.newbookmark').closest('section').toggleClass('read');
+  $('.mark').on('click', function() {
+    $(this).closest('section').toggleClass('read');
     console.log('read');
   });
-  //
-  // $('.remove').on('click', function(event) {
-  //   $('section.newbookmark').remove()
-  // })
+
+  $('.delete').on('click', function() {
+    $(this).closest('section').remove();
+  });
+  
 });
